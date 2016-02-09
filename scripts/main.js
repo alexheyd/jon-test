d3f0087d4756d2fac4f231dca231697ea352ec1b
Alex Heyd <alex@gaikai.com>
2016-02-08 18:00:44 -0800

    Added modal example, added some CSS layout examples, using vh and vw.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
 jQuery(function($) {
-    $('.toggle-text').on('click', function() {
-        $('.main-copy').slideToggle();
+    // makes the text slide up and down
+    $('.toggle-text').on('click', function(event) {
+        // this means, prevent the default action the browser will take when you click the link (which is to go to the url specified in the "href")
+        event.preventDefault();
+        $('.intro-copy').slideToggle();
+    });
+
+    $('.open-modal').on('click', function(event) {
+        event.preventDefault();
+        $('.ui-block').show();
+        $('.modal').fadeIn();
+    });
+
+    $('.modal button').on('click', function(event) {
+        event.preventDefault();
+        $('.ui-block').hide();
+        $('.modal').fadeOut();
     });
 });
